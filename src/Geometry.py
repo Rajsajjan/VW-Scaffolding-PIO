650ae04bce102cb6a7f1bddc17f86eaa58904b49
Basavaraj <basavarajsajjan2003@gmail.com>
2026-06-27 10:49:51 +0530

    Add scaffold grid geometry

--- src/Geometry.py
+++ src/Geometry.py
@@ -1,1 +1,33 @@
-# Geometry functions for the Scaffolding Plug-in
+"""
+Geometry functions
+Layher Scaffolding Generator
+"""
+
+import math
+
+
+class Point3D:
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+def scaffold_grid(bays_x, bays_y, bay_length, bay_width):
+    """
+    Create grid points for scaffold standards.
+    """
+
+    points = []
+
+    for x in range(bays_x + 1):
+        for y in range(bays_y + 1):
+            points.append(
+                Point3D(
+                    x * bay_length,
+                    y * bay_width,
+                    0.0
+                )
+            )
+
+    return points# Geometry functions for the Scaffolding Plug-in
