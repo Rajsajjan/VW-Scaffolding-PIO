adb68146794dc3b5f69f5c0d47e97937e5a2f1a0
Basavaraj <basavarajsajjan2003@gmail.com>
2026-06-27 10:49:12 +0530

    Add default scaffold parameters
    Removed the Parameters class and added default values for bay dimensions and levels.

--- src/Parameters.py
+++ src/Parameters.py
@@ -1,42 +1,11 @@
-# Parameters for the Scaffolding Plug-in
+"""
+Scaffolding Plug-in Parameters
+"""
 
-import vs
+DEFAULT_BAY_LENGTH = 2.07
+DEFAULT_BAY_WIDTH = 0.73
+DEFAULT_LIFT_HEIGHT = 2.00
 
-
-class Parameters:
-    """
-    Stores all user-controlled scaffold settings.
-    These will later connect to Vectorworks PIO fields.
-    """
-
-    # Basic scaffold dimensions
-    BAY_LENGTH = 2000      # distance between vertical frames (mm)
-    BAY_WIDTH = 1000       # scaffold depth (mm)
-
-    # Vertical settings
-    LEVEL_HEIGHT = 2000    # height between lifts (mm)
-    NUMBER_OF_LEVELS = 3   # how many scaffold levels
-
-    # Base settings
-    BASE_JACK_HEIGHT = 150
-
-    # Safety settings
-    HAS_GUARDRAIL = True
-    HAS_TOEBOARD = True
-
-    # Optional settings
-    INCLUDE_LADDER = True
-
-    @staticmethod
-    def get_total_height():
-        """
-        Returns total scaffold height.
-        """
-        return Parameters.LEVEL_HEIGHT * Parameters.NUMBER_OF_LEVELS
-
-    @staticmethod
-    def get_total_length(num_bays):
-        """
-        Returns total scaffold length based on number of bays.
-        """
-        return Parameters.BAY_LENGTH * num_bays# Parameters for the Scaffolding Plug-in
+DEFAULT_BAYS_X = 3
+DEFAULT_BAYS_Y = 2
+DEFAULT_LEVELS = 2
