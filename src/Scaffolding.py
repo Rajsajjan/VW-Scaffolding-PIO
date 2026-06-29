b10e0b39feb00ea47efdf61263fc57ebbf28d38a
Basavaraj <basavarajsajjan2003@gmail.com>
2026-06-29 11:36:47 +0530

    Update Scaffolding.py

--- src/Scaffolding.py
+++ src/Scaffolding.py
@@ -1,48 +1,55 @@
 import vs
 from SymbolLibrary import STANDARDS, LEDGERS
 
+
 def place_symbol(symbol_name, x, y):
     if vs.GetObject(symbol_name):
         vs.Symbol(symbol_name, x, y, 0)
     else:
         vs.AlrtDialog("Symbol not found: " + symbol_name)
 
+
 def place_ledger(symbol_name, x, y, rotation):
     if vs.GetObject(symbol_name):
         vs.Symbol(symbol_name, x, y, rotation)
+    else:
+        vs.AlrtDialog("Ledger not found: " + symbol_name)
+
 
 def main():
-    # Read Plug-in Parameters
-bays_x = int(vs.PBaysX)
-bays_y = int(vs.PBaysY)
 
     # Read Plug-in Parameters
+    bays_x = int(vs.PBaysX)
+    bays_y = int(vs.PBaysY)
+
     standard_choice = vs.PStandardType
     ledger_choice = vs.PLedgerType
-    
+
     standard_symbol = STANDARDS[standard_choice]
     ledger_symbol = LEDGERS[ledger_choice]
 
     bay_length = vs.PBayLength
     bay_width = vs.PBayWidth
 
-    # Create corner points for every bay
+    # Create scaffold
     for ix in range(bays_x + 1):
         for iy in range(bays_y + 1):
 
             x = ix * bay_length
             y = iy * bay_width
 
+            # Vertical
             place_symbol("Layher ZB Adjustiable Base 20", x, y)
             place_symbol("Layher TR Base Collar", x, y)
             place_symbol(standard_symbol, x, y)
 
-           # X Direction
-           if ix < bays_x:
-               place_ledger(ledger_symbol, x, y + 190, 0)
+            # X Direction Ledger
+            if ix < bays_x:
+                place_ledger(ledger_symbol, x, y + 190, 0)
 
-           # Y Direction
-           if iy < bays_y:
-               place_ledger(ledger_symbol, x, y + 190, 90)
-               
-main() 
+            # Y Direction Ledger
+            if iy < bays_y:
+                place_ledger(ledger_symbol, x, y + 190, 90)
+
+
+main()
